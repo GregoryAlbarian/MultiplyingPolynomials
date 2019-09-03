ee3b95729caa989077328e30e476c290db080a8a
GregoryAlbarian <albarian@chapman.edu>
2019-09-02 19:16:32 -0700

    created print_polynomial function

--- multiplying_polynomials.py
+++ multiplying_polynomials.py
@@ -1,3 +1,21 @@
+"""
+takes in a polynomial as a list where the indices are the exponent on the value
+print out the  polynomials in a form that looks reasonable
+"""
+def print_polynomial (polynomial):
+	for k in range(len(answer)-1,-1,-1):
+		if k==0:
+			print(answer[k]," + ",end="")
+		elif answer[k]==0:
+			continue
+		else:
+			print(answer[k],"x^",k,end="")
+
+		if k!=0:
+			print(" + ",end="")
+	print("")
+
+
 polynomial1 = [1,6,7]
 polynomial2 = [3, 0, 2, 3]
 
@@ -13,18 +31,7 @@ for i in range(0, len(polynomial1)):
 	#I need += because different exponents can still add up to the same number
 
 print(answer)
-for k in range(len(answer)-1,-1,-1):
-	if k==0:
-		print(answer[k],end="")
-	elif answer[k]==0:
-		continue
-	else:
-		print(answer[k],"x^",k,end="")
-
-	if k!=0:
-		print(" + ",end="")
-print("")
-#put on Github
+print_polynomial(answer)
 #put the bottom part in as a method for a second commit
 #make a third commit for user input
 #make a shell script for like a go to Desktop or go to hard drive
